54a3f8e3b62983d80a4c9eac7bf1c1551ac218a3
Mruthul M <94045372+mruthul-m@users.noreply.github.com>
2024-01-10 22:26:57 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,13 +5,7 @@ import App from "./App.jsx";
 import { SearchContextProvider } from "./contexts/SearchContext.jsx";
 import { AuthContextProvider } from "./contexts/AuthContext.jsx";
 
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   rootElement
-// );
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
